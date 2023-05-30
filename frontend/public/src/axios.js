28f9d0dfe81f14bc4154a0e0d9aab85e46a2d599
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-05-30 18:41:55 -0300

    .

--- frontend/public/src/axios.js
+++
@@ -1,9 +0,0 @@
-axios.get('https://reciturbackend.onrender.com/')
-  .then(response => {
-    // Manipule os dados da resposta aqui
-    console.log(response.data);
-  })
-  .catch(error => {
-    // Manipule os erros aqui
-    console.error(error);
-  });
