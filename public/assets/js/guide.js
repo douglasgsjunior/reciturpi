d14c40320ddcfcd290075f43312a8099028bbb07
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-05-31 07:47:50 -0300

    .

--- public/assets/js/guide.js
+++ public/assets/js/guide.js
@@ -1,38 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('https://reciturbackend.onrender.com/')
-    .then(response => response.json())
-    .then(data => {
-      const guideInfo = document.getElementById('guide-info');
-      
-      // Iterar sobre os dados do guia e criar elementos para exibir as informações
-      data.forEach(guide => {
-        const guideElement = document.createElement('div');
-        guideElement.classList.add('guide');
-        
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = guide.guide_name;
-        guideElement.appendChild(nameElement);
-        
-        const photoElement = document.createElement('img');
-        photoElement.src = guide.guide_photo;
-        guideElement.appendChild(photoElement);
-        
-        const descriptionElement = document.createElement('p');
-        descriptionElement.textContent = guide.guide_description;
-        guideElement.appendChild(descriptionElement);
-        
-        const locationElement = document.createElement('p');
-        locationElement.textContent = guide.guide_location;
-        guideElement.appendChild(locationElement);
-        
-        const whatsappElement = document.createElement('p');
-        whatsappElement.textContent = guide.guide_whatsapp_number;
-        guideElement.appendChild(whatsappElement);
-        
-        guideInfo.appendChild(guideElement);
-      });
-    })
-    .catch(error => {
-      console.error('Erro ao buscar informações do guia:', error);
-    });
-});
+const points = [
+  {
+    name: "Buraco da Véia",
+    info: "Brasília Teimosa, Recife - PE",
+    position: { lat: -8.079506, lng: -34.876120 },
+    category: "Praias",
+    image: "images/placeholder.png",
+    description: "O Buraco da Véia é uma praia localizada em Brasília Teimosa, Recife. É conhecida por sua atmosfera movimentada.",
+  },
+  {
+    name: "Praia de Brasília Teimosa",
+    info: "Brasília Teimosa, Recife - PE",
+    position: { lat: -8.084127, lng: -34.877668 },
+    category: "Praias",
+    image: "images/placeholder.png",
+    description: "A Praia de Brasília Teimosa é uma praia situada em Brasília Teimosa, Recife. É um local popular para atividades ao ar livre e esportes aquáticos.",
+  },
+  {
+    name: "Praia do Pina",
+    info: "Pina, Recife - PE",
+    position: { lat: -8.098343, lng: -34.883140 },
+    category: "Praias",
+    image: "images/placeholder.png",
+    description: "A Praia do Pina é uma praia localizada no bairro do Pina, em Recife. É conhecida por suas águas calmas e pela presença de quiosques e bares ao longo da orla.",
+  },
+  {
+    name: "Praia de Boa Viagem",
+    info: "Boa Viagem, Recife - PE",
+    position: { lat: -8.126293, lng: -34.897412 },
+    category: "Praias",
+    image: "images/placeholder.png",
+    description: "A Praia de Boa Viagem é uma das praias mais famosas e frequentadas de Recife. Possui uma extensa faixa de areia, piscinas naturais e uma movimentada orla com restaurantes e hotéis.",
+  },
+  {
+    name: "Shopping Recife",
+    info: "R. Padre Carapuceiro, 777 - Boa Viagem, Recife - PE, 51020-900",
+    position: { lat: -8.119343, lng: -34.904798 },
+    category: "Centros de Compras",
+    image: "images/placeholder.png",
+    description: "O Shopping Recife é um dos maiores centros de compras da cidade. Oferece uma ampla variedade de lojas, restaurantes, cinemas e entretenimento para os visitantes.",
+  }
+]
