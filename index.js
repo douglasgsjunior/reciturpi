ae0426c0b7d4a0dd482af318ecadcb552a508738
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-05-30 22:52:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = process.env.PORT || 3000; // Use a porta definida pelo ambiente ou a porta 3000 como fallback
+const port = 3000; // Use a porta definida pelo ambiente ou a porta 3000 como fallback
 
 const connection = mysql.createConnection({
   host: 'br9ikcswbolcedzmeuqh-mysql.services.clever-cloud.com',
