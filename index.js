994a4a043f6f846132e95c18d8b12c367aa3d95f
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-05-31 00:00:38 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000; // Use a porta definida pelo ambiente ou a porta 3000 como fallback
+const port = process.env.PORT || 3000; // Use a porta definida pelo ambiente ou a porta 3000 como fallback
 
 const connection = mysql.createConnection({
   host: 'br9ikcswbolcedzmeuqh-mysql.services.clever-cloud.com',
@@ -17,7 +17,7 @@ connection.on('connect', () => {
 });
 
 // Rota para buscar os dados no banco de dados
-app.get('/dados', (req, res) => {
+app.get('/', (req, res) => {
   connection.query('SELECT guide_name, guide_photo, guide_description, guide_location, guide_whatsapp_number FROM guide_tbl', (err, results) => {
     if (err) {
       console.error('Erro ao consultar o banco de dados:', err);
@@ -26,10 +26,6 @@ app.get('/dados', (req, res) => {
     }
     res.json(results);
   });
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
 });
 
 // Inicie o servidor
