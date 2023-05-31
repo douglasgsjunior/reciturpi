d3a61cbca9f28b89597313d0c7c0be3aa9fd1ca4
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-05-30 22:12:18 -0300

    .

--- public/assets/js/guide.js
+++ public/assets/js/guide.js
@@ -1,68 +1,38 @@
-const infoPage = document.getElementById("page3");
-
-// Função para criar um botão com base nos dados retornados do banco de dados
-function createButton(point) {
-  const newButton = document.createElement("button");
-  newButton.className = "page2-point";
-  newButton.id = point.guide_name.replace(/\s+/g, "-").toLowerCase() + "-button";
-
-  const leftContainer = document.createElement("div");
-  leftContainer.className = "left";
-
-  const imageContainer = document.createElement("div");
-  imageContainer.className = "image-container";
-
-  const image = document.createElement("img");
-  image.src = point.guide_photo;
-  image.alt = "ReciTur Logo";
-
-  imageContainer.appendChild(image);
-  leftContainer.appendChild(imageContainer);
-
-  const rightContainer = document.createElement("div");
-  rightContainer.className = "right";
-
-  const nameContainer = document.createElement("div");
-  nameContainer.className = "name-container";
-  nameContainer.textContent = point.guide_name;
-
-  const infoContainer = document.createElement("div");
-  infoContainer.className = "info-container";
-  infoContainer.textContent = point.guide_location;
-
-  const descContainer = document.createElement("div");
-  descContainer.className = "desc-container";
-  descContainer.textContent = point.guide_description;
-
-  rightContainer.appendChild(nameContainer);
-  rightContainer.appendChild(infoContainer);
-  rightContainer.appendChild(descContainer);
-
-  newButton.appendChild(leftContainer);
-  newButton.appendChild(rightContainer);
-
-  // Redirecionar para o WhatsApp ao clicar no botão
-  newButton.addEventListener('click', () => {
-    window.location.href = `https://api.whatsapp.com/send?phone=${point.guide_whatsapp_number}`;
-  });
-
-  return newButton;
-}
-
-// Função para buscar os dados do backend e criar os botões na página
-function fetchDataAndCreateButtons() {
+document.addEventListener('DOMContentLoaded', function() {
   fetch('https://reciturbackend.onrender.com/')
     .then(response => response.json())
     .then(data => {
-      data.forEach(point => {
-        const button = createButton(point);
-        infoPage.appendChild(button);
+      const guideInfo = document.getElementById('guide-info');
+      
+      // Iterar sobre os dados do guia e criar elementos para exibir as informações
+      data.forEach(guide => {
+        const guideElement = document.createElement('div');
+        guideElement.classList.add('guide');
+        
+        const nameElement = document.createElement('h3');
+        nameElement.textContent = guide.guide_name;
+        guideElement.appendChild(nameElement);
+        
+        const photoElement = document.createElement('img');
+        photoElement.src = guide.guide_photo;
+        guideElement.appendChild(photoElement);
+        
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = guide.guide_description;
+        guideElement.appendChild(descriptionElement);
+        
+        const locationElement = document.createElement('p');
+        locationElement.textContent = guide.guide_location;
+        guideElement.appendChild(locationElement);
+        
+        const whatsappElement = document.createElement('p');
+        whatsappElement.textContent = guide.guide_whatsapp_number;
+        guideElement.appendChild(whatsappElement);
+        
+        guideInfo.appendChild(guideElement);
       });
     })
     .catch(error => {
-      console.error('Erro ao buscar os dados do servidor:', error);
+      console.error('Erro ao buscar informações do guia:', error);
     });
-}
-
-// Chamar a função para buscar os dados e criar os botões
-fetchDataAndCreateButtons();
+});
